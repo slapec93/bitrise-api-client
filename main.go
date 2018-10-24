57520b8cb4ff490d0c538e3598d614df3e03d36e
Gergely Bekesi <gergely.bekesi@bitrise.io>
2018-10-24 15:01:04 +0200

    Implement client generation

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/bitrise-io/go-utils/log"
+)
 
 func main() {
-	fmt.Println("hello")
+	request, err := http.NewRequest("GET", "https://bitrise-team.github.io/bitrise-api/docs/swagger/swagger.json", nil)
+	if err != nil {
+		log.Errorf("Failed to initialize a new request: %s", err.Error())
+	}
+	client := http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Errorf("Failed to fetch swagger.json: %s", err.Error())
+	}
+
+	var swaggerData []byte
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusOK {
+		responseBody, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Errorf("Failed to read response body: %s", err.Error())
+		}
+		swaggerData = responseBody
+	}
+	f, err := os.Create("swagger.json")
+	if err != nil {
+		log.Errorf("Failed to create swagger.json: %s", err.Error())
+	}
+	defer f.Close()
+	_, err = f.Write(swaggerData)
+	if err != nil {
+		log.Errorf("Failed to save swagger.json: %s", err.Error())
+	}
 }

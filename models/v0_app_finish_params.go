e05ac0d83d851c8828149fd09497a5aee3679a36
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2019-08-01 22:03:07 +0000

    Update client

--- models/v0_app_finish_params.go
+++ models/v0_app_finish_params.go
@@ -15,19 +15,19 @@ import (
 // swagger:model v0.AppFinishParams
 type V0AppFinishParams struct {
 
-	// Which config to use default-android-config, default-cordova-config, default-fastlane-config, default-ionic-config, default-ios-config,default-macos-config, default-react-native-config, default-xamarin-config, other-config (default if parameter is not speficied)
+	// Which config to use `default-android-config`, `default-cordova-config`, `default-fastlane-config`, `default-ionic-config`, `default-ios-config`,`default-macos-config`, `default-react-native-config`, `default-xamarin-config`, `other-config` (default if parameter is not speficied)
 	Config string `json:"config,omitempty"`
 
 	// Environment variables for the application workflows, e.g. {"env1":"val1","env2":"val2"}
 	Envs interface{} `json:"envs,omitempty"`
 
-	// config specification mode, has to be specified with manual value
+	// config specification mode, has to be specified with `manual` value
 	Mode string `json:"mode,omitempty"`
 
 	// The slug of the organization, who will be the owner of the application, if it's not specified, then the authenticated user will be the owner
 	OrganizationSlug string `json:"organization_slug,omitempty"`
 
-	// The type of your project (android, ios, cordova, other, xamarin, macos, ionic, react-native, fastlane, null)
+	// The type of your project (`android`, `ios`, `cordova`, `other`, `xamarin`, `macos`, `ionic`, `react-native`, `fastlane`, null)
 	ProjectType string `json:"project_type,omitempty"`
 
 	// The id of the stack the application will be built (these can be found in the [system report](https://github.com/bitrise-io/bitrise.io/tree/master/system_reports) file names)
